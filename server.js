3bc7724ae373edcfdf6069ec6ecaff4dd4ef092f
Moriarty <ahmedehsouna@yahoo.com>
2020-01-03 11:15:03 +0100

    database connected

--- server.js
+++ server.js
@@ -4,5 +4,23 @@ var express       = require("express"),
     mongoose      = require("mongoose"),
     server        = express(),
     port          = process.env.PORT || 3000
+    mongoose.connect('mongodb://see7een:see7een@ds111279.mlab.com:11279/talking-circles', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  mongoose.connection.once("open" , _=> console.log("workin"))
+
+
+
+
 server.use(express.static("public"))
-      .listen(port)
+      
+
+server.get("/data" , (req, res)=>{
+
+      })
+
+
+
+
+server.listen(port)
